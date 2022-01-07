9354b3e41a4817e0b526874587f4178a0229a241
cxh1231 <363538880@qq.com>
2022-01-07 19:58:18 +0800

    :tada: 完善JustAuth登录服务.

--- excite-component/src/main/java/com/zxdmy/excite/component/bo/JustAuthBO.java
+++ excite-component/src/main/java/com/zxdmy/excite/component/bo/JustAuthBO.java
@@ -6,9 +6,7 @@ import lombok.experimental.Accessors;
 import java.io.Serializable;
 
 /**
- * <p>
- * 描述
- * </p>
+ * 第三方登录的配置信息实体
  *
  * @author 拾年之璐
  * @since 2022/1/6 17:30
@@ -19,13 +17,43 @@ public class JustAuthBO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 本条登录类型的KEY，如：qq、weibo等
+     */
     private String key;
 
+    /**
+     * 客户端id：对应各平台的appKey
+     */
     private String appID;
 
+    /**
+     * 客户端Secret：对应各平台的appSecret
+     */
     private String appSecret;
 
+    /**
+     * 登录成功后的回调地址
+     */
     private String redirectUri;
 
+    /**
+     * 只针对qq登录生效：是否需要申请unionid
+     */
     private Boolean unionId;
+
+    /**
+     * 企业微信第三方授权用户类型：member|admin
+     */
+    private String usertype;
+
+    /**
+     * 企业微信登录：授权方的网页应用ID
+     */
+    private String agentId;
+
+    /**
+     * 支付宝的公钥，只针对支付宝、阿里云登陆有效
+     */
+    private String publicKey;
 }
--- excite-component/src/main/java/com/zxdmy/excite/component/justauth/JustAuthService.java
+++ excite-component/src/main/java/com/zxdmy/excite/component/justauth/JustAuthService.java
@@ -1,25 +1,25 @@
 package com.zxdmy.excite.component.justauth;
 
+import cn.hutool.core.util.DesensitizedUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zxdmy.excite.common.exception.ServiceException;
 import com.zxdmy.excite.common.service.IGlobalConfigService;
 import com.zxdmy.excite.component.bo.JustAuthBO;
 import lombok.AllArgsConstructor;
 import me.zhyd.oauth.config.AuthConfig;
-import me.zhyd.oauth.enums.scope.AuthBaiduScope;
 import me.zhyd.oauth.enums.scope.AuthGiteeScope;
-import me.zhyd.oauth.enums.scope.AuthWeiboScope;
+import me.zhyd.oauth.model.AuthCallback;
+import me.zhyd.oauth.model.AuthResponse;
+import me.zhyd.oauth.model.AuthUser;
 import me.zhyd.oauth.request.*;
 import me.zhyd.oauth.utils.AuthScopeUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
- * <p>
- * 描述
- * </p>
+ * 第三方登录服务接口
  *
  * @author 拾年之璐
  * @since 2022/1/6 12:02
@@ -35,33 +35,77 @@ public class JustAuthService {
     private static final Set<String> CONF_KEY_SET = new HashSet<String>() {{
         add("qq");
         add("weibo");
-        add("gitee");
         add("wechat_open");
         add("wechat_mp");
         add("wechat_enterprise");
         add("baidu");
+        add("dingtalk");
+        add("aliyun");
+        add("alipay");
+        add("huawei");
+        add("feishu");
+        add("gitee");
+        add("github");
     }};
 
 
-    public boolean saveConfig(JustAuthBO authBO) {
-        if (CONF_KEY_SET.contains(authBO.getKey())) {
+    /**
+     * 保存配置信息
+     *
+     * @param authBO 配置信息类
+     * @return 保存结果
+     */
+    public boolean saveConfig(JustAuthBO authBO) throws JsonProcessingException {
+        // 配置信息的key必须存在于当前的库中
+        if (null != authBO.getKey() && CONF_KEY_SET.contains(authBO.getKey())) {
+            // 三大必填信息不能为空
             if (null == authBO.getAppID() || null == authBO.getAppSecret() || null == authBO.getRedirectUri()) {
-
+                throw new ServiceException("APPID、APPSecret和RedirectUri不能为空！请检查！");
+            } else {
+                return configService.save(DEFAULT_SERVICE, authBO.getKey(), authBO, true);
             }
-            return false;
         } else {
             throw new ServiceException("当前输入的配置KEY[" + authBO.getKey() + "]不在许可范围内，请检查！");
         }
     }
 
+    /**
+     * 根据配置的KEY获取一条配置信息
+     *
+     * @param confKey 配置的KEY
+     * @return 指定KEY的配置信息
+     */
     public JustAuthBO getConfig(String confKey) {
         JustAuthBO justAuthBO = new JustAuthBO();
         return (JustAuthBO) configService.get(DEFAULT_SERVICE, confKey, justAuthBO);
     }
 
     /**
+     * 获取所有的登录配置信息
+     *
+     * @param desensitized 是否脱敏
+     * @return 配置列表
+     */
+    public List<JustAuthBO> getConfigList(Boolean desensitized) {
+        JustAuthBO justAuthBO = new JustAuthBO();
+        List<Object> objectList = configService.getList(DEFAULT_SERVICE, justAuthBO);
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<JustAuthBO> justAuthBOList = new ArrayList<>();
+        for (Object o : objectList) {
+            justAuthBO = objectMapper.convertValue(o, justAuthBO.getClass());
+            if (desensitized) {
+                justAuthBO.setAppSecret(DesensitizedUtil.idCardNum(justAuthBO.getAppSecret(), 2, 2));
+                justAuthBO.setPublicKey(DesensitizedUtil.idCardNum(justAuthBO.getPublicKey(), 2, 2));
+            }
+            justAuthBOList.add(justAuthBO);
+        }
+        return justAuthBOList;
+    }
+
+    /**
+     * 生成登录授权的URL
+     *
      * @param confKey 配置的KEY，也即授权来源（如qq、weibo等）
-     * @param confKey 配置的KEY
      * @param state   状态码（字符串）
      * @return 登录的链接
      */
@@ -70,14 +114,33 @@ public class JustAuthService {
     }
 
     /**
+     * 根据回调信息获取登录用户的信息
+     *
+     * @param confKey  配置的KEY，也即授权来源（如qq、weibo等）
+     * @param callback 接口回调信息
+     * @return 授权成功后的用户信息，根据授权平台的不同，获取的数据完整性也不同
+     */
+    public AuthUser getAuthUser(String confKey, AuthCallback callback) {
+        AuthRequest authRequest = this.createAuthRequest(confKey);
+        // 根据返回的参数，执行登录请求（获取用户信息）
+        AuthResponse<AuthUser> response = authRequest.login(callback);
+        // 判断结果
+        if (response.ok()) {
+            return response.getData();
+        } else {
+            return null;
+        }
+    }
+
+    /**
      * 根据具体地授权来源，获取授权请求工具类
      *
      * @param confKey 配置的KEY，也即授权来源（如qq、weibo等）
-     * @return
+     * @return 授权工具类
      */
     public AuthRequest createAuthRequest(String confKey) {
         JustAuthBO authBO = this.getConfig(confKey);
-        AuthRequest authRequest = null;
+        AuthRequest authRequest;
         switch (confKey) {
             // QQ登录
             case "qq":
@@ -85,6 +148,7 @@ public class JustAuthService {
                         .clientId(authBO.getAppID())
                         .clientSecret(authBO.getAppSecret())
                         .redirectUri(authBO.getRedirectUri())
+                        .unionId(authBO.getUnionId())
                         .build());
                 break;
             // 微博登录
@@ -93,20 +157,6 @@ public class JustAuthService {
                         .clientId(authBO.getAppID())
                         .clientSecret(authBO.getAppSecret())
                         .redirectUri(authBO.getRedirectUri())
-                        .scopes(Arrays.asList(
-                                AuthWeiboScope.EMAIL.getScope(),
-                                AuthWeiboScope.FRIENDSHIPS_GROUPS_READ.getScope(),
-                                AuthWeiboScope.STATUSES_TO_ME_READ.getScope()
-                        ))
-                        .build());
-                break;
-            // Gitee登录
-            case "gitee":
-                authRequest = new AuthGiteeRequest(AuthConfig.builder()
-                        .clientId(authBO.getAppID())
-                        .clientSecret(authBO.getAppSecret())
-                        .redirectUri(authBO.getRedirectUri())
-                        .scopes(AuthScopeUtils.getScopes(AuthGiteeScope.values()))
                         .build());
                 break;
             // 微信开放平台登录
@@ -131,7 +181,7 @@ public class JustAuthService {
                         .clientId(authBO.getAppID())
                         .clientSecret(authBO.getAppSecret())
                         .redirectUri(authBO.getRedirectUri())
-                        .agentId("1000003")
+                        .agentId(authBO.getAgentId())
                         .build());
                 break;
             // 百度登录
@@ -140,16 +190,79 @@ public class JustAuthService {
                         .clientId(authBO.getAppID())
                         .clientSecret(authBO.getAppSecret())
                         .redirectUri(authBO.getRedirectUri())
-                        .scopes(Arrays.asList(
-                                AuthBaiduScope.BASIC.getScope(),
-                                AuthBaiduScope.SUPER_MSG.getScope(),
-                                AuthBaiduScope.NETDISK.getScope()
-                        ))
+                        .build());
+                break;
+            // 钉钉登录
+            case "dingtalk":
+                authRequest = new AuthDingTalkRequest(AuthConfig.builder()
+                        .clientId(authBO.getAppID())
+                        .clientSecret(authBO.getAppSecret())
+                        .redirectUri(authBO.getRedirectUri())
+                        .build());
+                break;
+            // 阿里云登录
+            case "aliyun":
+                authRequest = new AuthAliyunRequest(AuthConfig.builder()
+                        .clientId(authBO.getAppID())
+                        .clientSecret(authBO.getAppSecret())
+                        .redirectUri(authBO.getRedirectUri())
+                        .alipayPublicKey(authBO.getPublicKey())
+                        .build());
+                break;
+            // 支付宝登录
+            case "alipay":
+                authRequest = new AuthAlipayRequest(AuthConfig.builder()
+                        .clientId(authBO.getAppID())
+                        .clientSecret(authBO.getAppSecret())
+                        .redirectUri(authBO.getRedirectUri())
+                        .alipayPublicKey(authBO.getPublicKey())
+                        .build());
+                break;
+            // 华为登录
+            case "huawei":
+                authRequest = new AuthHuaweiRequest(AuthConfig.builder()
+                        .clientId(authBO.getAppID())
+                        .clientSecret(authBO.getAppSecret())
+                        .redirectUri(authBO.getRedirectUri())
+                        .build());
+                break;
+            // 飞书登录
+            case "feishu":
+                authRequest = new AuthFeishuRequest(AuthConfig.builder()
+                        .clientId(authBO.getAppID())
+                        .clientSecret(authBO.getAppSecret())
+                        .redirectUri(authBO.getRedirectUri())
+                        .build());
+                break;
+            // Gitee登录
+            case "gitee":
+                authRequest = new AuthGiteeRequest(AuthConfig.builder()
+                        .clientId(authBO.getAppID())
+                        .clientSecret(authBO.getAppSecret())
+                        .redirectUri(authBO.getRedirectUri())
+                        .scopes(AuthScopeUtils.getScopes(AuthGiteeScope.values()))
+                        .build());
+                break;
+            // GitHub登录
+            case "github":
+                authRequest = new AuthGithubRequest(AuthConfig.builder()
+                        .clientId(authBO.getAppID())
+                        .clientSecret(authBO.getAppSecret())
+                        .redirectUri(authBO.getRedirectUri())
                         .build());
                 break;
             default:
                 throw new ServiceException("配置的KEY不存在，请检查！");
         }
         return authRequest;
+    }
+
+    /**
+     * 读取当前auth服务支持的KEY的集合
+     *
+     * @return 支持的KEY的集合
+     */
+    public Set<String> getConfKeySet() {
+        return CONF_KEY_SET;
     }
 }
